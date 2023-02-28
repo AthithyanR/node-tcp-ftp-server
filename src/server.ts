7422445ab09d657051b00f6b807031a313e0604e
Athithyan Rengaswamy <atr@Athithyans-MacBook-Pro.local>
2023-02-28 12:48:00 +0530

    tcp server initial setup done

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('hello')
+import { createServer, Socket } from "node:net";
+
+const tcpServer = createServer();
+
+tcpServer.on("close", () => console.log("tcpServer closed"))
+tcpServer.on("connection", (sock: Socket) => {
+    setInterval(() => sock.write("Hello there!\n"), 1000);
+})
+tcpServer.on("drop", (e) => console.log("tcpServer drop", e))
+tcpServer.on("error", (e) => console.log("tcpServer error", e))
+tcpServer.on("listening", () => console.log(`tcpServer listening on: ${JSON.stringify(tcpServer.address())}`))
+
+tcpServer.listen(process.env.PORT || 6969);
